7483123acf191462fca441bf10ed1319480756fd
Carl Alexander <carlalexander@gmail.com>
2020-08-10 00:12:07 -0400

    test: use faker `domainName` instead of `url`

--- tests/Unit/Subscriber/WordPressSubscriberTest.php
+++ tests/Unit/Subscriber/WordPressSubscriberTest.php
@@ -23,36 +23,36 @@ class WordPressSubscriberTest extends TestCase
 {
     public function testEnableUrlRewriteWithOtherServerSoftware()
     {
-        $this->assertFalse((new WordPressSubscriber('PHP', $this->faker->url))->enableUrlRewrite(false));
+        $this->assertFalse((new WordPressSubscriber('PHP', 'https://'.$this->faker->domainName))->enableUrlRewrite(false));
     }
 
     public function testEnableUrlRewriteWithYmirServerSoftware()
     {
-        $this->assertTrue((new WordPressSubscriber('YMIR', $this->faker->url))->enableUrlRewrite(false));
+        $this->assertTrue((new WordPressSubscriber('YMIR', 'https://'.$this->faker->domainName))->enableUrlRewrite(false));
     }
 
     public function testEnableVisualEditorWithOtherServerSoftware()
     {
-        $this->assertFalse((new WordPressSubscriber('PHP', $this->faker->url))->enableVisualEditor(false));
+        $this->assertFalse((new WordPressSubscriber('PHP', 'https://'.$this->faker->domainName))->enableVisualEditor(false));
     }
 
     public function testEnableVisualEditorWithYmirServerSoftware()
     {
-        $this->assertTrue((new WordPressSubscriber('YMIR', $this->faker->url))->enableVisualEditor(false));
+        $this->assertTrue((new WordPressSubscriber('YMIR', 'https://'.$this->faker->domainName))->enableVisualEditor(false));
     }
 
     public function testRewritePluginUrlOnlyKeepsDirectoryBelowPlugins()
     {
-        $siteUrl = $this->faker->url;
+        $siteUrl = 'https://'.$this->faker->domainName;
 
-        $this->assertSame($siteUrl.'/directory/plugins/test.php', (new WordPressSubscriber('PHP', $siteUrl))->rewritePluginUrl($this->faker->url.'/foo/directory/plugins/test.php'));
+        $this->assertSame($siteUrl.'/directory/plugins/test.php', (new WordPressSubscriber('PHP', $siteUrl))->rewritePluginUrl('https://'.$this->faker->domainName.'/foo/directory/plugins/test.php'));
     }
 
     public function testSanitizeFileNameCharacters()
     {
         $this->assertSame(
             ['?', '[', ']', '/', '\\', '=', '<', '>', ':', ';', ',', "'", '"', '&', '$', '#', '*', '(', ')', '|', '~', '`', '!', '{', '}', '+', chr(0)],
-            (new WordPressSubscriber('YMIR', $this->faker->url))->sanitizeFileNameCharacters()
+            (new WordPressSubscriber('YMIR', 'https://'.$this->faker->domainName))->sanitizeFileNameCharacters()
         );
     }
 }
