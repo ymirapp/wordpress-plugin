72ddd65fd3931319f3e87f6d7c6bcd01786ccd74
Carl Alexander <carlalexander@gmail.com>
2021-09-18 12:11:39 -0400

    refactor: how query monitor integration gets configured

--- src/Configuration/QueryMonitorConfiguration.php
+++ src/Configuration/QueryMonitorConfiguration.php
@@ -29,29 +29,50 @@ class QueryMonitorConfiguration implements ContainerConfigurationInterface
     public function modify(Container $container)
     {
         $container['query_monitor_active'] = $container->service(function () {
-            $isActive = is_plugin_active('query-monitor/query-monitor.php');
-
-            if ($isActive) {
-                require_once WP_CONTENT_DIR.'/plugins/query-monitor/classes/Collector.php';
-                require_once WP_CONTENT_DIR.'/plugins/query-monitor/classes/Collectors.php';
-                require_once WP_CONTENT_DIR.'/plugins/query-monitor/classes/Output.php';
-                require_once WP_CONTENT_DIR.'/plugins/query-monitor/output/Html.php';
+            if (!is_plugin_active('query-monitor/query-monitor.php')) {
+                return false;
             }
 
-            return $isActive;
+            $files = [
+                WP_CONTENT_DIR.'/plugins/query-monitor/classes/Collector.php',
+                WP_CONTENT_DIR.'/plugins/query-monitor/classes/Collectors.php',
+                WP_CONTENT_DIR.'/plugins/query-monitor/classes/Output.php',
+                WP_CONTENT_DIR.'/plugins/query-monitor/output/Html.php',
+            ];
+
+            if (!array_filter($files, 'file_exists')) {
+                return false;
+            }
+
+            foreach ($files as $file) {
+                require_once $file;
+            }
+
+            return true;
+        });
+        $container['query_monitor_display_object_cache_active'] = $container->service(function () {
+            return wp_using_ext_object_cache() && !is_plugin_active('object-cache-pro/redis-cache-pro.php');
         });
         $container['query_monitor_collectors'] = $container->service(function (Container $container) {
-            return [
-                $container['query_monitor_object_cache_collector'],
-            ];
+            $collectors = [];
+
+            if ($container['query_monitor_display_object_cache_active']) {
+                $collectors[] = $container['query_monitor_object_cache_collector'];
+            }
+
+            return $collectors;
         });
         $container['query_monitor_object_cache_collector'] = $container->service(function (Container $container) {
             return new ObjectCacheCollector($container['wp_object_cache']);
         });
-        $container['query_monitor_panels'] = $container->service(function () {
-            return [
-                ObjectCachePanel::class,
-            ];
+        $container['query_monitor_panels'] = $container->service(function (Container $container) {
+            $panels = [];
+
+            if ($container['query_monitor_display_object_cache_active']) {
+                $panels[] = ObjectCachePanel::class;
+            }
+
+            return $panels;
         });
     }
 }
