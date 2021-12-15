6996e202dc8372632dd5f1d978cb9487820010d8
Carl Alexander <carlalexander@gmail.com>
2021-12-15 13:08:50 -0500

    chore: bump version

--- ymir.php
+++ ymir.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * Plugin Name: Ymir
  * Plugin URI: https://ymirapp.com
  * Description: Integrates WordPress with the Ymir platform.
- * Version: 1.11.3
+ * Version: 1.11.4
  * Author: Carl Alexander
  * Author URI: https://ymirapp.com
  * License: GPL3
