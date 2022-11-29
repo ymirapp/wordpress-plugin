8454d5ea0e450748a0de7a95047fe18044c0b3ca
Carl Alexander <carlalexander@gmail.com>
2022-11-29 13:18:12 -0500

    feat: remove object cache from plugin

--- src/Configuration/EventManagementConfiguration.php
+++ src/Configuration/EventManagementConfiguration.php
@@ -59,9 +59,6 @@ class EventManagementConfiguration implements ContainerConfigurationInterface
                 new Subscriber\Compatibility\WpMigrateDbSubscriber(),
             ];
 
-            if ($container['query_monitor_active']) {
-                $subscribers[] = new Subscriber\QueryMonitorSubscriber($container['query_monitor_collectors'], $container['query_monitor_panels'], $container['plugin_dir_path'].'/resources/views/query-monitor');
-            }
             if ($container['ymir_cdn_image_processing_enabled']) {
                 $subscribers[] = new Subscriber\ContentDeliveryNetworkImageProcessingSubscriber($container['image_sizes'], $container['is_multisite'], $container['uploads_baseurl'], $container['content_width']);
             }
